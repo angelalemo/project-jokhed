08b7dabf7fb77f3d3a7e8e8a794df80d2f35cb75
Naruedon-mu <marknarudon@gmail.com>
2026-01-02 11:52:42 +0700

    picture update

--- src/database/people-database/init/02_seed_data.sql
+++ src/database/people-database/init/02_seed_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO people (name, nickname, phone_number, image_url)
 VALUES
-    ('นฤดล เมืองอุดร', 'มาร์ค', '084-367-7079', NULL),
-    ('สมหญิง', 'หญิง', '0898765432', NULL),
-    ('John Doe', 'John', '0900000001', NULL);
+    ('Naruedon Mueangaudon', 'Mark', '084-367-7079', 'uploads/people/peoplepicture'),
+    ('Yudsapak Panyapeng', 'Golf', '0898765432', 'uploads/people/peoplepicture'),
+    ('Krittayou Karwkanlaya', 'Google', '0900000001', 'uploads/people/peoplepicture');
