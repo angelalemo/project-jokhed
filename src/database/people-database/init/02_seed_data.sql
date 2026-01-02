bffb0f4e9b2f66756833a05390817fab76d51054
Naruedon-mu <marknarudon@gmail.com>
2026-01-02 15:14:27 +0700

    fix with test

--- src/database/people-database/init/02_seed_data.sql
+++ src/database/people-database/init/02_seed_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO people (name, nickname, phone_number, image_url)
 VALUES
-    ('Naruedon Mueangaudon', 'Mark', '084-367-7079', 'uploads/people/peoplepicture'),
-    ('Yudsapak Panyapeng', 'Golf', '0898765432', 'uploads/people/peoplepicture'),
-    ('Krittayou Karwkanlaya', 'Google', '0900000001', 'uploads/people/peoplepicture');
+    ('Naruedon Mueangaudon', 'Mark', '084-367-7079', '/uploads/people/mark.jpg'),
+    ('Yudsapak Panyapeng', 'Golf', '0898765432', '/uploads/people/golf.jpg'),
+    ('Krittayou Karwkanlaya', 'Google', '0900000001', '/uploads/people/Google.jpg');
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,16 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // Enable CORS for frontend
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   // Serve uploaded files so they are accessible at /uploads/... (e.g., images)
-  app.useStaticAssets(join(process.cwd(), 'uploads'));
-  await app.listen(process.env.PORT ?? 4000);
+  app.useStaticAssets(join(process.cwd(), 'uploads'), {
+    prefix: '/uploads/',
+  });
+  await app.listen(4000);
 }
 bootstrap();
